95f71142e9b69a780173fd8c6783291ef0b60463
rhgarcia88 <rhgarguer@gmail.com>
2024-06-27 19:35:11 +0200

    Fix vercel config

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ connectDB();
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
-  api_secret: process.env.API_secret
+  api_secret: process.env.API_SECRET
   
 });
 
